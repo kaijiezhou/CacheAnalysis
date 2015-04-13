be7581ab20762f46da030d2d503b41142c529bb1
kaijiezhou <kaijiezhou913@gmail.com>
2015-04-12 18:28:03 -0700

    delete

--- block_size_analysis.c
+++ block_size_analysis.c
@@ -4,8 +4,6 @@ int main(){
 	time_t start,end;
 	start = time(NULL);
 	printf("hello");
-	printf(" world!!!!\n");
-	printf(" world!!!!\n");
 	end = time(NULL);
 	printf("The time is %f\n",difftime(end,start));
 	return 1;
