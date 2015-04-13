73cf5ad9f77be6dcd0f917a37f8b1dc46ab4b239
kaijiezhou <kaijiezhou913@gmail.com>
2015-04-12 18:54:08 -0700

    fix conflicts

--- block_size_analysis.c
+++ block_size_analysis.c
@@ -4,8 +4,6 @@ int main(){
 	time_t start,end;
 	start = time(NULL);
 	printf("hello");
-	printf("hello");
-	printf("ZKJSB");
 	printf("ZKJSB");
 	end = time(NULL);
 	printf("The time is %f\n",difftime(end,start));
