744ba9861a60f821b8bd552d58598ca024b2f8d3
duwentaoabc <duwentaoabc@gmail.com>
2015-04-13 11:48:23 -0700

    fix

--- block_size_analysis.c
+++ block_size_analysis.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 int main(){
 	time_t start,end;
+	struct timespec tstart={0,0},tend={0,0};
 	start = time(NULL);
-	printf("hello");
-	printf(" world!!!!\n");
-	printf(" world!!!!\n");
+	clock_gettime(CLOCK_MONOTONIC,&tstart);
+	int* p = malloc(1024 * 1024 * 100 / 4 * sizeof(int));
+	memset(p,0,1024 * 1024 * 25);
+	long i =0;
+	for(i = 0;i<1024;i++){
+	    printf("%ld->%d\n",i,(*(p + i * 1024 * 25)));
+	}
 	end = time(NULL);
+	clock_gettime(CLOCK_MONOTONIC,&tend);
 	printf("The time is %f\n",difftime(end,start));
 	return 1;
 }
