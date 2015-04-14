97a9ca80f56bb9fca39b3d4ef2abf15f0c219720
kaijiezhou <kaijiezhou913@gmail.com>
2015-04-13 19:33:19 -0700

    use clock instrad of sec

--- DataCachesAnls.c
+++ DataCachesAnls.c
@@ -3,7 +3,7 @@
 #include<time.h>
 #include<malloc.h>
 
-double readCache(int *a, long start, long end){
+long readCache(int *a, long start, long end){
 	int i,tmp;
 	clock_t sc,ec;
 	sc=clock();
@@ -11,29 +11,36 @@ double readCache(int *a, long start, long end){
 		tmp=a[i];
 	}
 	ec=clock();
-	double t=(double)(ec-sc)/CLOCKS_PER_SEC;
-	printf("run time of walking %ld-%ld is %f sec\n",start,end,t);
+	long t=(long)(ec-sc);
+	printf("run time of walking %ld-%ld is %ld cycles\n",start,end,t);
 	return t;
 }
 
 int main(){
 	long length=pow(2,14)/4;
 	long indexAt8K=pow(2,13)/4;
+	clock_t start,end;
 	int *a=(int *)malloc(sizeof(int)*length);
 	int *b=(int *)malloc(sizeof(int)*length);
 	long i;
+	start=clock();
 	for(i=0;i<length;i++){
 		a[i]=0;
 	}
+	end=clock();
+	printf("first go a[]: %ld cycles\n",end-start);
+	start=clock();
 	for(i=0;i<length;i++){
 		b[i]=1;
 	}
-	double t1=readCache(a,0,indexAt8K);
-	double t2=readCache(a,indexAt8K,length);
-	double t3=readCache(a,0,indexAt8K);
-	if (t1==t3)
-		printf("Size of Data Cache is 8K!\n");
-	else 
-		printf("Size of Data Cache is 16K!\n");
+	end=clock();
+	printf("second go b[]:%ld cycles\n", end-start);
+	long t1=readCache(a,0,indexAt8K);
+	long t2=readCache(a,indexAt8K,length);
+	long t3=readCache(a,0,indexAt8K);
+	//if (t1==t3)
+	//	printf("Size of Data Cache is 8K!\n");
+	//else 
+	//	printf("Size of Data Cache is 16K!\n");
 	return 1;
 }
