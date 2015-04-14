080a84fd187daa2cca80a6c821e98b37319653e9
duwentaoabc <duwentaoabc@gmail.com>
2015-04-14 16:32:23 -0700

    fix blockSize

--- blockSize.c
+++ blockSize.c
@@ -8,16 +8,12 @@ int writeData(int *data){
     return 0;
 }
 
-int readData(int *data,int initial, int size, int slot){
-    int i,temp;
-}
-
-int readData(int *data,int initial, int size, int slot){
+int readData(int *data, int size, int slot){
     int i,temp;
     struct timeval sc,tc;
     gettimeofday(&sc,NULL);
     for(i =0;i<size;i++){
-        temp = *(data + initial + i * slot);
+        temp = *(data + i * slot);
     }
     gettimeofday(&tc,NULL);
     unsigned long time = 1000000 * (tc.tv_sec - sc.tv_sec) + tc.tv_usec -
@@ -29,10 +25,10 @@ int readData(int *data,int initial, int size, int slot){
 int main(){                                                   
         int *data = (int *)malloc(sizeof(int) * 1<<(WRITE_DATA_SIZE - 2));  
         writeData(data);                                      
-        readData(data,0,READ_DATA_SIZE,1 << MAX_SLOT);        
+        readData(data,READ_DATA_SIZE,1 << MAX_SLOT);        
         int i;                                                
         for(i = 0; i < MAX_SLOT;i++){                         
-        readData(data,((i+1) * (1 << (MAX_SLOT << 1))) ,READ_DATA_SIZE,(1 << i));                                                             
+        readData(data + ((i+1) * (1 << (MAX_SLOT << 1))) ,READ_DATA_SIZE,(1 << i));                                                             
         }
         return 0;
 }
