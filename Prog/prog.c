51585f4b287c8946233f40a60df3113c018a4f80
kaijiezhou <kaijiezhou913@gmail.com>
2015-04-21 20:07:18 -0700

    dfsaf

--- Prog/prog.c
+++ Prog/prog.c
@@ -12,7 +12,7 @@ static inline unsigned ccnt_read (void)
           return cc;
 }
 
-unsigned c1, c2 ;
+unsigned c1, c2, time;
 
 void counter_start(void)
 {
@@ -22,28 +22,67 @@ void counter_start(void)
 void counter_stop(void)
 {
       c2 = ccnt_read();
+        time += (c2 - c1) / 100000;
 }
 
+void counter_print(void)
+{
+    printf("%d\n", time);      
+}
+
+void write_prog1(int *data, int loopNumner, int size){
+    int i,j;
+    for(i = 0;i<loopNumner;++i){
+        reset();
+        counter_start();
+        for (int j = 0; j < size; ++j)
+        {
+            (*(data + j))++;
+        }
+        counter_stop();
+    }
+    counter_print();
+    return;
+}
+
+void write_prog2(int *data, int loopNumner, int size){
+    int i,j;
+    for(i = 0;i<loopNumner;++i){
+        reset();
+        for (int k = 0; k < 8; ++k)
+        {
+            counter_start();
+            for (int j = k; j < size; j=j+8)
+            {
+                (*(data + j))++;
+            }
+            counter_stop();
+        }
+    }
+    counter_print();
+    return;
+}
+
+void freshMemory(int *data, int size){
+    for (int j = 0; j < size; ++j){
+            *(data + j) = 0;;
+        }
+}
 int main(int argc, char *argv[]){
-	int i;
+	time = 0;
     int j;
-	int *data ;
+    int *data ;
     unsigned adj;
     adj = 32 - ((unsigned)data%32);
     data = data +adj;
-    data= (int *)malloc(sizeof(int) * (1 <<(WRITE_DATA_SIZE-2)) * 10 +64);
+    data= (int *)alloca(sizeof(int) * (1 <<(WRITE_DATA_SIZE-2)) * 10 +64);
     int arrary_size = (1 <<(WRITE_DATA_SIZE-2)) * 10;
-    counter_start();
-    memset(data,0,(1<<(WRITE_DATA_SIZE-2)) * 10);
+    init_ccr();
     for(j = 1; j<=1000; j = j*10){
-    	while(*data < j){
-    	   for (i =arrary_size-1; i>=0;i--){
-    	   *(data+i) = *(data+i) +1;
-    	   }  
-    	}
-    	printf("%d\n",*data);  
+        freshMemory(data,arrary_size);
+        write_prog1(data,j,arrary_size);
+        freshMemory(data,arrary_size);
+        write_prog2(data,j,arrary_size);
     }
-    counter_stop();
-    printf("Total time for prog1 is %u s",c2-c1)
-	return 0;
+    return 0;
 }
